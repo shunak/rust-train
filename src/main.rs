465bd9c8c947638228a42d99f609564613c07738
shunak <shuna966@gmail.com>
2021-08-09 18:08:37 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -89,20 +89,47 @@ fn main() {
         fn area(&self) -> u32 {
             self.width * self.height
         }
+        fn can_hold(&self, other: &Rectangle)->bool {
+            self.width > other.width && self.height > other.height
+        }
     }
-    
+
+
+
     let rect1 = Rectangle{ width:30, height:50};
+    let rect2 = Rectangle{ width:10, height:40};
+    let rect3 = Rectangle{ width:60, height:45};
+    
 
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
         );
 
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter,
+    }
+
+        fn value_in_cents(coin: Coin) -> u32 {
+            match coin {
+                Coin::Penny => 1,
+                Coin::Nickel=> 5,
+                Coin::Dime=> 10,
+                Coin::Quarter=> 25,
+            }
+        }
+
+        let coin_value = value_in_cents(Coin::Dime);
 
 
 
-
-
+        println!("{}",coin_value);
 
 }
 
