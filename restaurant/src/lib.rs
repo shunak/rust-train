dac2b4ec73935fa836c015fa910e0ac4af881a70
shunak <shuna966@gmail.com>
2021-08-15 18:02:19 +0900

    wip

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -44,7 +44,9 @@ mod back_of_house {
 }
 
 
-use crate::front_of_house::hosting;
+// use crate::front_of_house::hosting;
+
+pub use crate::front_of_house::hosting;
 
 fn serve_order() {}
 
@@ -72,11 +74,29 @@ pub fn eat_at_restaurant(){
 }
 
 
+use std::fmt;
+// use std::io;
+
+// fn function1() -> fmt::Result {
+// }
+
+// fn function2() -> io::Result<()>{
+// }
 
 
+use std::fmt::Result;
+// use std::io::Result as IoResult;
+
+// fn function3() -> Result {
+
+// }
+// fn function4() -> IoResult<()>{
+// }
 
 
+// use std::{cmp::Ordering, io};
 
+use std::io::{self, Write};
 
-
+use std::collections::*;
 
