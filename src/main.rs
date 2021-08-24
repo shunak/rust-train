964920a516064ae4e63a84a99c7f6499e602780d
shunak <shuna966@gmail.com>
2021-08-25 00:11:13 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,24 @@ use std::collections::HashMap;
 
 fn main() {
         
+
+    
+
+    
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"),10);
+    scores.insert(String::from("Yellow"),80);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+    println!("{:?}",score);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+
     let field_name = String::from("Favorite color");
     let field_value = String::from("Blue");
 
