d6cc5439398109b6d7f8182eace5a4766e25af67
shunak <shuna966@gmail.com>
2021-08-28 22:32:58 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 use std::collections::HashMap;
 use std::fs::File;
+use std::io;
 use std::io::ErrorKind;
+use std::io::Read;
+
 
 fn main() {
     // let f: u32 = File::open("hello.txt");
+
+    // let f = File::open("hello.txt").unwrap();
+    // let f = File::open("hello.txt").expect("Faild to open hello.txt");
+    read_username_from_file();
+        
+    
+
+
+
+
     let f = File::open("hello.txt");
 
     // let f = match f {
@@ -368,3 +381,19 @@ fn first_word(s: &String) -> &str {
     &s[..]
 }
 
+
+fn read_username_from_file() -> Result<String, io::Error>{
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
