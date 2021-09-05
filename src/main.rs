9775737feef51441c71709c9a582d5bb2ad893c8
shunak <shuna966@gmail.com>
2021-09-05 23:02:38 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,32 @@ use std::io::ErrorKind;
 use std::io::Read;
 use rust_train::{Summary, Tweet, NewsArticle};
 
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T{
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+
+}
+
+
 fn main() {
+
+    let number_list = vec![34,50,25,100,65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y','m','a','q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+
+
 
     let tweet = Tweet {
         username: String::from("horse_ebooks"),
