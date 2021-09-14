2218025c25c1b566f7ed041aa92b10806aa1b563
shunak <shuna966@gmail.com>
2021-09-14 22:09:38 +0900

    wip

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -17,9 +17,33 @@ pub fn greeting(name: &str) -> String{
     format!("Hello {}!", name)
 }
 
+pub struct Guess {
+    value: i32,
+}
+impl Guess {
+    pub fn new(value: i32) -> Guess{
+        if value < 1  {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        // if value < 1 || value > 100 {
+        //     panic!("Guess value must be between 1 and 100, got {}.", value);
+        // }
+        Guess { value }
+
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100(){
+        Guess::new(200);
+    }
 
     #[test]
     fn greeting_contains_name(){
@@ -28,33 +52,33 @@ mod tests {
     }
 
 
-    #[test]
-    fn it_adds_two(){
-        assert_eq!(4, add_two(2));
-    }
+    // #[test]
+    // fn it_adds_two(){
+    //     assert_eq!(4, add_two(2));
+    // }
     
 
     #[test]
     fn exploration() {
         assert_eq!(2 + 2, 4);
     }
-    #[test]
-    fn another(){
-        panic!("Make this test fail");
-    }
+    // #[test]
+    // fn another(){
+    //     panic!("Make this test fail");
+    // }
 
-    #[test]
-    fn larger_can_hold_smaller(){
-        let larger = Rectangle{
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle{
-            width: 5,
-            height: 1,
-        };
-        assert!(larger.can_hold(&smaller));
-    }
+    // #[test]
+    // fn larger_can_hold_smaller(){
+    //     let larger = Rectangle{
+    //         width: 8,
+    //         height: 7,
+    //     };
+    //     let smaller = Rectangle{
+    //         width: 5,
+    //         height: 1,
+    //     };
+    //     assert!(larger.can_hold(&smaller));
+    // }
 
     #[test]
     fn smaller_cannot_hold_larger(){
