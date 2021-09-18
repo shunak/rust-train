051cd53a4ac3d684a115d3695e58eb687163cae1
shunak <shuna966@gmail.com>
2021-09-19 00:16:23 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,16 @@ use std::io::Read;
 use rust_train::{Summary, Tweet, NewsArticle};
 use std::fmt;
 use std::fmt::Display;
+use std::thread;
+use std::time::Duration;
 
+
+fn simulated_expensive_calculation(intensity: u32) -> u32{
+    // calculate slowly
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
 
 fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
     where T: Display
@@ -41,6 +50,15 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 }
 
 fn main() {
+
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    );
+
     // #[derive(Debug)]
      struct Word{
          x: String,
