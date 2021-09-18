5ae832d57f25bd8f60c6fb5b2ff5dcbf66316384
shunak <shuna966@gmail.com>
2021-09-18 23:58:45 +0900

    wip

---
+++ adder/target/debug/deps/common-4f9688cc166a5eac.d
@@ -0,0 +1,5 @@
+/root/rust-train/adder/target/debug/deps/common-4f9688cc166a5eac: tests/common.rs
+
+/root/rust-train/adder/target/debug/deps/common-4f9688cc166a5eac.d: tests/common.rs
+
+tests/common.rs:
