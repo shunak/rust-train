54b5804e121a1efeb9bd44093f955b2b95acf97d
shunak <shuna966@gmail.com>
2021-09-20 23:54:51 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,33 @@ fn simulated_expensive_calculation(intensity: u32) -> u32{
     intensity
 }
 
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
+impl <T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32)->u32{
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
+}
 
 fn generate_workout(intensity: u32, random_number:u32){
     let expensive_closure = |num: u32| {
