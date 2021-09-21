124f089a122ca0098c44f3fe9fb4931808703b82
shunak <shuna966@gmail.com>
2021-09-21 23:47:51 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -134,6 +134,14 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 }
 
 fn main() {
+    let x = 4;
+
+    let equal_to_x = |z| z == x;
+
+    let y = 4;
+
+    assert!(equal_to_x(y));
+
 
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
