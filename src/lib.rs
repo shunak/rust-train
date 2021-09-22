d27a48f5331fee1982986a00f3e6dde79ff7feea
shunak <shuna966@gmail.com>
2021-09-23 03:30:51 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -81,16 +81,29 @@ fn iterator_demonstration(){
 
     let v1 = vec![1,2,3];
     let mut v1_iter = v1.iter();
+    // println!("{:?}",v1_iter.next());
     assert_eq!(v1_iter.next(), Some(&1));
     assert_eq!(v1_iter.next(), Some(&2));
+    // println!("{:?}",v1_iter.next());
     assert_eq!(v1_iter.next(), Some(&3));
+    // println!("{:?}",v1_iter.next());
     assert_eq!(v1_iter.next(), None);
+    // println!("{:?}",v1_iter.next());
 
 }
 
 
 
+#[test]
+fn iterator_sum() {
+    let v1 = vec![1,2,3];
 
+    let v1_iter = v1.iter();
+
+    let total: i32 = v1_iter.sum();
+
+    assert_eq!(total, 6);
+}
 
 
 
