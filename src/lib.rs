3edf0538929b13202e824d1b3137866f40a22eb5
shunak <shuna966@gmail.com>
2021-09-23 03:17:33 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -76,7 +76,17 @@ impl<T: Display + PartialOrd> Pair<T> {
 }
 
 
+#[test]
+fn iterator_demonstration(){
 
+    let v1 = vec![1,2,3];
+    let mut v1_iter = v1.iter();
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+
+}
 
 
 
