27a9520d87604c5e18edd94c388984391a0405c6
shunak <shuna966@gmail.com>
2021-09-23 03:39:22 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -106,4 +106,12 @@ fn iterator_sum() {
 }
 
 
+#[test]
+fn iterator_with_map() {
+    let v1: Vec<i32> = vec![1,2,3];
 
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect(); // closure preserve state
+
+    assert_eq!(v2, vec![2,3,4]);
+
+}
