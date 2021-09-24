2894dad7c9a76dbec6f55224d15eb865ebc3cf5d
shunak <shuna966@gmail.com>
2021-09-24 23:33:46 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -137,8 +137,7 @@ fn filters_by_size() {
         Shoe {size: 13, style: String::from("sandal")},
         Shoe {size: 10, style:String::from("boot")},
     ];
-    let in_my_size = shoes_in_my_size(shoes, 13);
-    
+    let in_my_size = shoes_in_my_size(shoes, 10);
     assert_eq!(
         in_my_size,
         vec![
@@ -146,10 +145,6 @@ fn filters_by_size() {
             Shoe {size: 10, style: String::from("boot")}
         ]
     );
-
-
-
-
 }
 
 
@@ -162,8 +157,41 @@ impl Counter {
         Counter {count:0}
     }
 }
+impl Iterator for Counter {
+    type Item = u32;
+    fn next(&mut self)-> Option<Self::Item>{
+        self.count +=1;
+
+        if self.count < 6 {
+            Some(self.count)
+        }else{
+            None
+        }
+    }
+}
 
 
+#[test]
+fn calling_next_directly(){
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(),Some(1));
+    assert_eq!(counter.next(),Some(2));
+    assert_eq!(counter.next(),Some(3));
+    assert_eq!(counter.next(),Some(4));
+    assert_eq!(counter.next(),Some(5));
+    assert_eq!(counter.next(),None);
+}
+
+
+#[test]
+fn using_other_iterator_trait_methods(){
+    let sum: u32 = Counter::new().zip(Counter::new().skip(1))
+                                .map(|(a,b)| a * b)
+                                .filter(|x| x % 3 ==0)
+                                .sum();
+    assert_eq!(18, sum);
+}
 
 
 
