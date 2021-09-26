d24969da44f9644935d6a3bd23d415a07b112fb4
shunak <shuna966@gmail.com>
2021-09-27 04:29:45 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -222,4 +222,14 @@ impl Config {
 }
 
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
 
+    for line in contents.lines(){
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+
+    results
+}
