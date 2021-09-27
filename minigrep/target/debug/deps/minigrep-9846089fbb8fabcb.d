a83d2f9b028a75795c167afdfaa28207ef904a57
shunak <shuna966@gmail.com>
2021-09-28 04:30:14 +0900

    wip

---
+++ minigrep/target/debug/deps/minigrep-9846089fbb8fabcb.d
@@ -0,0 +1,5 @@
+/root/rust-train/minigrep/target/debug/deps/minigrep-9846089fbb8fabcb: src/main.rs
+
+/root/rust-train/minigrep/target/debug/deps/minigrep-9846089fbb8fabcb.d: src/main.rs
+
+src/main.rs:
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/root/rust-train/minigrep/target/debug/minigrep: /root/rust-train/minigrep/src/main.rs
