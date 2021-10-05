2c300fca8b9c320d1e69e52efe58e78adbe49376
shunak <shuna966@gmail.com>
2021-10-05 18:01:41 +0900

    wip

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -43,5 +43,18 @@ impl Config {
 #[cfg(test)]
 mod test {
     use super::*;
+    #[test]
+    fn one_result(){
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        pick three.";
 
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+            );
+    }
 }
