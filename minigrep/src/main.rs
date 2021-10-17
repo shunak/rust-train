af4807d6189368f60004efd66bc87a6f409165c3
shunak <shuna966@gmail.com>
2021-10-18 00:17:31 +0900

    wip

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,14 +8,14 @@ use minigrep::Config;
 
 fn main() {
 
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
     // let filename = &args[2];
     // let query = &args[1];
     // let (query, filename) = parse_config(&args);
 
     // let config = parse_config(&args);
-    let config = Config::new(&args).unwrap_or_else(|err|{
+    let config = Config::new(env::args()).unwrap_or_else(|err|{
         eprintln!("Problem parsing arguments:{}",err);
         process::exit(1);
     });
