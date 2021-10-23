aaf87851d29a269f9b53c99fca94caa28ca825ed
shunak <shuna966@gmail.com>
2021-10-24 00:41:22 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -205,27 +205,3 @@ assert_eq!(v2, vec![2, 3, 4]);
 }
 
 
-/// Adds one to the number given.
-/// 与えられた数値に1を足す。
-///
-/// # Examples
-///
-/// ```
-/// let five = 5;
-///
-/// assert_eq!(6, my_crate::add_one(5));
-/// ```
-pub fn add_one(x: i32) -> i32 {
-    x + 1
-}
-
-
-
-
-
-
-
-
-
-
-
