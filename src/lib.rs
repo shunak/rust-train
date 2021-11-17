c03c4b380cb0645aa90c2527c22daf38f232879e
shunak <shuna966@gmail.com>
2021-11-17 23:30:39 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -246,21 +246,21 @@ assert_eq!(v2, vec![2, 3, 4]);
 #[cfg(tet)]
 mod tests {
     use super::*;
-    // use std::cell:RefCell;
+    use std::cell:RefCell;
 
     struct MocMessenger{
-        sent_messages: Vec<String>,
+        sent_messages: efCell<Vec<String>>,
     }
 
     impl MockMessenger{
         fn new() -> MockMessenger {
-            MockMessenger { sent_messages: vec![]}
+            MockMessenger { sent_messages: RefCell::new(vec![])}
         }
     }
 
     impl Messenger for MockMessenger {
         fn send(&self, message: &str){
-            self.sent_messages.push(String::from(message));
+            self.sent_messages.borrow_mut().push(String::from(message));
         }
     }
 
@@ -272,7 +272,7 @@ mod tests {
 
         limit_tracker.set_value(80);
 
-        assert_eq!(mock_messenger.sent_messages.len(),1);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(),1);
     }
 
 }
