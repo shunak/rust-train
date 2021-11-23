d5dde3c116479f10c7b8009c78cff45a9a401d49
shunak <shuna966@gmail.com>
2021-11-23 22:11:56 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use std::process;
 use std::mem::drop;
 use List::{Cons, Nil};
 use std::rc::Rc;
-
+use std::cell::RefCell;
 
 fn simulated_expensive_calculation(intensity: u32) -> u32{
     // calculate slowly
@@ -138,12 +138,17 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     }
 }
 
+#[derive(Debug)]
+enum List{
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
 
-    #[derive(Debug)]
-    enum List {
-        Cons(i32, Rc<List>),
-        Nil,
-    }
+    // #[derive(Debug)]
+    // enum List {
+    //     Cons(i32, Rc<List>),
+    //     Nil,
+    // }
 
     // use List::{Cons, Nil};
 
@@ -163,18 +168,30 @@ fn main() {
     // let x = 5;
     // let y = &mut x;
 
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 
 
 
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-    }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    // let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    // println!("count after creating a = {}", Rc::strong_count(&a));
+    // let b = Cons(3, Rc::clone(&a));
+    // println!("count after creating b = {}", Rc::strong_count(&a));
+    // {
+    //     let c = Cons(4, Rc::clone(&a));
+    //     println!("count after creating c = {}", Rc::strong_count(&a));
+    // }
+    // println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 
 
     // let b = Cons(3, Rc::clone(&a));
@@ -797,6 +814,13 @@ fn read_username_from_file() -> Result<String, io::Error>{
         Err(e) => Err(e),
     }
 }
+
+
+
+
+
+
+
 
 
 
