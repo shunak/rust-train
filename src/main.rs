1caa0a18cd0d61d5dcd75aad407f31bb05750f09
shunak <shuna966@gmail.com>
2021-11-30 00:00:20 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ use std::cell::RefCell;
 struct Node {
     value: i32,
     parent: RefCell<Weak<Node>>,
-    children: RefCell<Vec<Rc<Node>>>.
+    children: RefCell<Vec<Rc<Node>>>,
 }
 
 
@@ -211,14 +211,22 @@ fn main() {
 
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
 
+    // parent of leaf = {:?}
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
     let branch = Rc::new(Node {
         value: 5,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
 
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 
     // let c = CustomSmartPointer{data: String::from("my stuff")};
     // let d = CustomSmartPointer{data: String::from("other stuff")};
