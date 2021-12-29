b99a9b617e3dbc2c8f46ad1a008d58c19e78f88f
shunak <shuna966@gmail.com>
2021-12-30 00:03:37 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -190,27 +190,48 @@ impl List {
 
 
 fn main() {
-    let counter = Mutex::new(0);
+    let counter = Rc::new(Mutex::new(0));
     let mut handles = vec![];
 
+    for _ in 0..10 {
+        let counter = Rc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
 
-    let handle = thread::spawn(move || {
-        let mut num = counter.lock().unwrap();
-
-        *num +=1;
-    });
-    handles.push(handle);
-
-    let handle2 = thread::spawn(move || {
-        let mut num2 = counter.lock().unwrap();
-
-        *num2 +=1;
-    });
-    handles.push(handle2);
+            *num +=1;
+        });
+        handles.push(handle);
+    }
 
     for handle in handles {
         handle.join().unwrap();
     }
+
+    println!("Result: {}", *counter.lock().unwrap());
+
+
+
+    // let counter = Mutex::new(0);
+    // let mut handles = vec![];
+
+
+    // let handle = thread::spawn(move || {
+    //     let mut num = counter.lock().unwrap();
+
+    //     *num +=1;
+    // });
+    // handles.push(handle);
+
+    // let handle2 = thread::spawn(move || {
+    //     let mut num2 = counter.lock().unwrap();
+
+    //     *num2 +=1;
+    // });
+    // handles.push(handle2);
+
+    // for handle in handles {
+    //     handle.join().unwrap();
+    // }
 
     // for _ in 0..10 {
     //     let handle = thread::spawn(move || {
@@ -224,7 +245,7 @@ fn main() {
     //     handle.join().unwrap();
     // }
 
-    println!("Result: {}", *counter.lock().unwrap());
+    // println!("Result: {}", *counter.lock().unwrap());
 
 
     // let m = Mutex::new(5);
