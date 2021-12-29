002a04434d6574a29f6319a71ae431fe1c6fd8f4
shunak <shuna966@gmail.com>
2021-12-29 23:46:50 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -190,17 +190,32 @@ impl List {
 
 
 fn main() {
-    // let counter = Mutex::new(0);
-    // let mut handles = vec![];
+    let counter = Mutex::new(0);
+    let mut handles = vec![];
 
-    let m = Mutex::new(5);
-
-    {
-        let mut num = m.lock().unwrap();
-        *num = 6;
+    for _ in 0..10 {
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
     }
 
-    println!("m = {:?}",m);
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+
+
+    // let m = Mutex::new(5);
+
+    // {
+    //     let mut num = m.lock().unwrap();
+    //     *num = 6;
+    // }
+
+    // println!("m = {:?}",m);
 
 
 
