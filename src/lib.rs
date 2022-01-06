d340af524aaca32515871e646383c75283e2e643
shunak <shuna966@gmail.com>
2022-01-06 23:50:21 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,30 @@ pub trait Draw {
     fn draw(&self);
 }
 
-pub struct Screen {
-    pub components: Vec<Box<Draw>>,
+// pub struct Screen {
+//     pub components: Vec<Box<Draw>>,
+// }
+pub struct Screen<T: Draw> {
+    pub components: Vec<T>,
 }
+impl<T> Screen<T> 
+    where T: Draw{
+    pub fn run(&self){
+        for component in self.components.iter(){
+            component.draw();
+        }
+    }
+}
+
+// impl Screen {
+//     pub fn run(&self){
+//         for component in self.components.iter(){
+//             component.draw();
+//         }
+//     }
+// }
+
+
 
 pub struct AveragedCollection {
     list: Vec<i32>,
