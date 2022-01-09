54f6894985c044753765d5ad62381fdc1b4e1508
shunak <shuna966@gmail.com>
2022-01-10 00:25:56 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,10 @@ use std::thread;
 use std::time::Duration;
 use std::sync::mpsc;
 use std::sync::{Mutex, Arc};
-extern crate gui;
+use gui::{Screen, Button};
 use gui::Draw;
 use gui::Screen;
+extern crate gui;
 
 struct SelectBox {
     width: u32,
@@ -271,6 +272,28 @@ impl List {
     }
 
 fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox{
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No")
+                ],
+            }),
+            Box::new(Button{
+                width: 50,
+                height: 10, label: String::from("OK"),
+            })
+        ],
+    };
+
+    screen.run();
+
+
+
     let screen = Screen {
         components: vec![
           Box::new(String::from("Hi")),  
