126c36146a9c9e7151fa39b51b405746fbc34359
shunak <shuna966@gmail.com>
2022-02-05 23:46:50 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -456,6 +456,24 @@ impl List {
     }
 
 fn main() {
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "24".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {}, as the backgroud", color);
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    }else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    }else {
+        println!("Using blue as the background color");
+    }
+
     let fibb: FbncBottomUp = FbncBottomUp{
         n: 10,
     };
