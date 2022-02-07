214e32b45ad999e14c31f6f7188a492a4be94d5d
shunak <shuna966@gmail.com>
2022-02-07 22:54:16 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -455,7 +455,17 @@ impl List {
         }
     }
 
+
+fn print_coordinates(&(x,y): &(i32,i32)){
+    println!("Current location: ({},{})", x, y);
+}
+
 fn main() {
+    let point = (3,5);
+    print_coordinates(&point);
+
+
+
     let v  = vec!['a','b','c'];
     for (index, value) in v.iter().enumerate(){
         println!("{} is at index {}", value, index);
