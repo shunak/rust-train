ee840bf9a7a259f2201350064914d7571bf3b207
shunak <shuna966@gmail.com>
2022-02-12 23:37:18 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -535,10 +535,18 @@ struct Point {
 }
 fn main() {
     let p = Point { x: 0, y: 7 };
-    let Point { x: a, y: b } = p;
-    assert_eq!(0 ,a);
-    assert_eq!(7 ,b);
+    // let Point { x: a, y: b } = p;
+    let Point { x, y} = p;
+    assert_eq!(0 ,x);
+    // assert_eq!(0 ,a);
+    assert_eq!(7 ,y);
+    // assert_eq!(7 ,b);
 
+    match p {
+        Point {x,y:0} => println!("On the x axis at {}", x),
+        Point {x:0, y} => println!("On the y axis at {}",y),
+        Point {x,y} => println!("On neither axis: ({}, {})", x, y),
+    }
 
 
 
