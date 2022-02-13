c97a2217a0eec0cecce45bf2931a0db210fb4e2a
shunak <shuna966@gmail.com>
2022-02-13 23:15:57 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -533,6 +533,14 @@ struct Point {
     x: i32,
     y: i32,
 }
+
+enum Message {
+    Quit, 
+    Move { x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32,i32,i32),
+}
+
 fn main() {
     let p = Point { x: 0, y: 7 };
     // let Point { x: a, y: b } = p;
