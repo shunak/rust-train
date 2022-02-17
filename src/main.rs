6615a690e051016b182b9f06513f022f2b27f3aa
shunak <shuna966@gmail.com>
2022-02-17 23:51:36 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -614,6 +614,23 @@ fn foo(_: i32, y:i32){
 }
 
 fn main() {
+
+let mut setting_value = Some(5);
+let new_setting_value = Some(10);
+
+match (setting_value, new_setting_value){
+    (Some(_), Some(_)) => {
+        println!("Can't overwrite an existing customized value");
+    }
+    _ => {
+        setting_value = new_setting_value;
+    }
+}
+
+println!("Setting is {:?}", setting_value);
+
+
+
 foo(3,4);
 
 
