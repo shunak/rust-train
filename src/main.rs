5e8e38f70569f1a6f6e664c0f0262abb7076894e
shunak <shuna966@gmail.com>
2022-02-22 23:02:54 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -622,6 +622,17 @@ struct Point3 {
 
 fn main() {
 
+let num = Some(4);
+
+match num {
+    Some(x) if x < 5 => println!("less than five: {}",x ),
+    Some(x) => println!("{}",x),
+    None => (),
+}
+
+
+
+
 let mut robot_name = Some(String::from("Bors"));
 
 match robot_name {
