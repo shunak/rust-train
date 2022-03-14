8b3cd1959f2001b1459aeba05f2adcce449a117a
shunak <shuna966@gmail.com>
2022-03-14 23:52:00 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,17 @@
 use std::fmt::Display;
 use std::env;
+use std::ops::Add;
+
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + other.0 * 1000)
+    }
+}
 
 
 pub struct Post {
