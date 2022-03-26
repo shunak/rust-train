b33c16b04201f5941521b89fde24b9abb7d20bdf
shunak <shuna966@gmail.com>
2022-03-26 22:45:05 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -833,7 +833,29 @@ impl Add for Points {
         }
     }
 
+    trait Animal {
+        fn baby_name() -> String;
+    }
+
+    struct Dog;
+
+    impl Dog {
+        fn baby_name() -> String {
+            String::from("Spot")
+        }
+    }
+
+    impl Animal for Dog {
+        fn baby_name() -> String {
+            String::from("puppy")
+        }
+    }
+
 fn main() {
+
+
+    println!("A baby dog is called a {}", Dog::baby_name());
+
 
     let person = Human;
     Pilot::fly(&person);
