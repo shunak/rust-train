f2f60431a4e5a054a3128353ce67254bd305e0d4
shunak <shuna966@gmail.com>
2022-03-30 22:21:23 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -914,8 +914,21 @@ impl fmt::Display for Wrapper {
     }
 }
 
-    
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+
+
 fn main() {
+    let answer = do_twice(add_one,5);
+    println!("The answer is: {}", answer);
+
+
+
 
     let noptrl_td: NumberOfPathsToReachLastCell_TD = NumberOfPathsToReachLastCell_TD{
         array: &vec![
