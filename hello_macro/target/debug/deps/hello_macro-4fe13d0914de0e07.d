5787892792525ea7b0e6d2754a88c83cfd38166a
shunak <shuna966@gmail.com>
2022-04-10 00:47:40 +0900

    wip

---
+++ hello_macro/target/debug/deps/hello_macro-4fe13d0914de0e07.d
@@ -0,0 +1,5 @@
+/Users/syu/rust-train/hello_macro/target/debug/deps/hello_macro-4fe13d0914de0e07: src/main.rs
+
+/Users/syu/rust-train/hello_macro/target/debug/deps/hello_macro-4fe13d0914de0e07.d: src/main.rs
+
+src/main.rs:
---
+++ hello_macro/target/debug/deps/hello_macro-93ea566238206db8.d
@@ -0,0 +1,7 @@
+/Users/syu/rust-train/hello_macro/target/debug/deps/hello_macro-93ea566238206db8.rmeta: src/lib.rs
+
+/Users/syu/rust-train/hello_macro/target/debug/deps/libhello_macro-93ea566238206db8.rlib: src/lib.rs
+
+/Users/syu/rust-train/hello_macro/target/debug/deps/hello_macro-93ea566238206db8.d: src/lib.rs
+
+src/lib.rs:
